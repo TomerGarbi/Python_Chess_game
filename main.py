344dd8af26583135f50f0485b5d2fe73401ba8f6
TomerGarbi <tomergar10@gmail.com>
2022-09-15 23:01:36 +0300

    draw board and pieces

--- main.py
+++ main.py
@@ -1,29 +1,58 @@
 import sys
-
+import chess
 import pygame
 pygame.init()
 
-WIDTH, HIGHT = 700, 500
-WIN = pygame.display.set_mode((WIDTH, HIGHT))
-pygame.display.set_caption("hello")
-FPS = 30
+WIDTH, HIGHT = 512, 512
+dim = 8
+square_size = HIGHT // dim
+FPS = 15
+images = {}
+
+def load_images():
+    pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bN", "bB", "bQ", "bK"]
+    for piece in pieces:
+        images[piece] = pygame.transform.scale(pygame.image.load(f"./Pieces/{piece}.png"), (square_size, square_size))
+
+
+def draw_board(screen):
+    colors = [pygame.Color("white"), pygame.Color("grey")]
+    for r in range(dim):
+        for c in range(dim):
+            color = colors[(r+c) % 2]
+            pygame.draw.rect(screen, color, pygame.Rect(c * square_size, r * square_size, square_size, square_size))
+
+
+
+def draw_pieces(screen, board):
+    for r in range(dim):
+        for c in range(dim):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(images[piece], pygame.Rect(c * square_size, r * square_size, square_size, square_size))
+
+
+def draw_game_state(screen, gs):
+    draw_board(screen)
+    draw_pieces(screen, gs.board)
+
+
 
 def main():
-    run = True
-    counter = 0;
+    screen = pygame.display.set_mode((WIDTH, HIGHT))
     clock = pygame.time.Clock()
+    gs = chess.GameState()
+    load_images()
+    run = True
     while run:
-        clock.tick(FPS)
-        counter += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_game_state(screen, gs)
+        clock.tick(FPS)
+        pygame.display.flip()
 
     pygame.quit()
-    print(counter)
-    sys.exit()
-
-
 
 
 if __name__ == "__main__":
