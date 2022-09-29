2d22e7c356777eca7da50e6f6678cdb661303cfe
TomerGarbi <tomergar10@gmail.com>
2022-09-29 18:05:40 +0300

    start screen

--- main.py
+++ main.py
@@ -7,11 +7,13 @@ WIDTH, HIGHT = 512, 512
 dim = 8
 square_size = HIGHT // dim
 FPS = 15
+clock = pygame.time.Clock()
 images = {}
-
+font = pygame.font.SysFont('Arial', 16)
 
 def load_images():
     pieces = ["wP", "wR", "wN", "wB", "wQ", "wK", "bP", "bR", "bN", "bB", "bQ", "bK"]
+    images["logo"] = pygame.transform.scale(pygame.image.load("./img/chess_game_icon.png"), (200, 200))
     for piece in pieces:
         images[piece] = pygame.transform.scale(pygame.image.load(f"./img/Pieces/{piece}.png"), (square_size, square_size))
     images["gray_dot"] = pygame.transform.scale(pygame.image.load("./img/gray_dot.png"), (square_size, square_size))
@@ -75,11 +77,43 @@ def wait_for_promotion_key():
                     return "B"
                 else:
                     print("press 'q', 'r', 'n' or 'b'")
+        clock.tick(FPS)
+
+#
+# def draw_open_screen(screen):
+#     pygame.display.
+
+def open_screen(screen):
+    buttons_size = (150, 50)
+    start_button_pos = (-75 + screen.get_width() // 2, -10 + screen.get_height() // 2)
+    run_open_screen = True
+    quit = False
+    while run_open_screen:
+        screen.fill((255, 255, 255))
+        location = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame. QUIT:
+                run_open_screen = False
+                quit = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if start_button_pos[0] <= location[0] <= start_button_pos[0] + buttons_size[0] and start_button_pos[1] <= location[1] <= buttons_size[1] + start_button_pos[1]:
+                    return 2
+        screen.blit(images["logo"], pygame.Rect(150, 25, 200, 200))
+        pygame.draw.rect(screen, (125, 125, 125), pygame.Rect(start_button_pos[0], start_button_pos[1], buttons_size[0], buttons_size[1]))
+        screen.blit(font.render('Start Game', True, (0, 0, 0)), (start_button_pos[0] + 32, start_button_pos[1] + 15))
+        pygame.display.update()
+        clock.tick(FPS)
+    if quit:
+        pygame.quit()
+        return 0
+    return 1
+
+
+
 
 
 def main():
     screen = pygame.display.set_mode((WIDTH, HIGHT))
-    clock = pygame.time.Clock()
     gs = chess.GameState()
     load_images()
     run = True
@@ -88,6 +122,10 @@ def main():
     possible_piece_moves = []
     valid_moves = gs.get_valid_moves()
     move_made = False
+    start_game = open_screen(screen)
+    if start_game == 0:
+        exit(0)
+
     while run:
         if gs.checkmate:
             run = False
